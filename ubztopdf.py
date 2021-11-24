7bead7eccc0fdf5ba87ac3b58b57c36014973658
Pesc0 <54720203+Pesc0@users.noreply.github.com>
2021-11-24 01:37:42 +0100

    Updated code to accept multiple files in command line

--- ubztopdf.py
+++ ubztopdf.py
@@ -1,38 +1,53 @@
 #!/usr/bin/env python3
 
-import zipfile
 import argparse
+import os
+import zipfile
 from svglib.svglib import svg2rlg
 from reportlab.graphics import renderPDF
 from reportlab.pdfgen import canvas
-import os
 import shutil
 
-def main():
-    parser = argparse.ArgumentParser(description='Convert .ubz files to pdf.')
-    parser.add_argument('Path', metavar='path', type=str, help='Path to .ubz file')
-    args = parser.parse_args()
-    input_path = args.Path
-    extracted_path = input_path[:-4]
+
+
+def ubz_to_pdf(filepath):
+    file_name, file_extension = os.path.splitext(filepath)
     
-    with zipfile.ZipFile(input_path, 'r') as archive:
-        archive.extractall(extracted_path)
+    if file_extension != '.ubz':
+        raise Exception('File ' + filepath + ' is not an ubz file. Skipping.')
 
-    filenames = sorted(os.listdir(extracted_path))
-    svgfiles = [extracted_path + '/' + file for file in filenames if file[-4:] == '.svg']
+    extract_dir = '/tmp/ubztopdf/' + os.path.basename(file_name)
 
-    xL, yL, xH, yH = svg2rlg(open(svgfiles[0], 'r')).getBounds()
-    pdf = canvas.Canvas(input_path[:-4] + '.pdf', pagesize = (xH - xL, yH - yL))
+    with zipfile.ZipFile(filepath, 'r') as archive:
+        archive.extractall(extract_dir)
+
+    allfiles = sorted(os.listdir(extract_dir))
+    svgfiles = [extract_dir + '/' + file for file in allfiles if file[-4:] == '.svg']
+
+    xL, yL, xH, yH = svg2rlg(svgfiles[0]).getBounds() 
+    pdf = canvas.Canvas(file_name + '.pdf', pagesize = (xH - xL, yH - yL))
 
     for file in svgfiles:
         drawing = svg2rlg(file)
-        #renderPDF.drawToFile(drawing, file)
+        #renderPDF.drawToFile(drawing, file) #Single image
         renderPDF.draw(drawing, pdf, 0, 0)
         pdf.showPage()
 
     pdf.save()
+    shutil.rmtree(extract_dir)
 
-    shutil.rmtree(extracted_path)
+
+def main():
+    parser = argparse.ArgumentParser(description='Convert .ubz files to pdf.')
+    parser.add_argument('files', nargs='+', type=str, help='One or more paths to .ubz files')
+    args = parser.parse_args()
+
+    for file in args.files:
+        try:
+            ubz_to_pdf(file)
+        except Exception as e:
+            print(str(e))
+
 
 if __name__ == "__main__":
     main()
